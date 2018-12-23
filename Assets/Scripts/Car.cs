c9cc2130bef41e059ca711bede6ec520dc521a18
James Ransome <jamesransome@msn.com>
2018-12-23 22:41:05 +0000

    Add Car

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public bool IsHumanControlled = false;
+    [SerializeField] private float torque = 800f;
+    [SerializeField] private float maxLockDegrees = 35f;
+    [SerializeField] private WheelCollider[] frontWheels;
+
+    public void Throttle(float input)
+    {
+        foreach (WheelCollider w in frontWheels)
+            w.motorTorque = input * torque;
+    }
+
+    public void Steer(float input)
+    {
+        foreach (WheelCollider w in frontWheels)
+            w.steerAngle = input * maxLockDegrees;
+    }
+
+    private void Update()
+    {
+        if (!IsHumanControlled) return;
+        Throttle(Input.GetAxis("Vertical"));
+        Steer(Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public float force = 10f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.left * force, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
