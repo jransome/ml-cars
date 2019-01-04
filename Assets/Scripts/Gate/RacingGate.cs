2fb5e374f1f114903dc91a944502c54fe830588b
jransome <jamesransome@msn.com>
2019-01-04 21:20:47 +0000

    Add RacingGates to track

--- Assets/Scripts/Gate/RacingGate.cs
+++ Assets/Scripts/Gate/RacingGate.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class RacingGate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform optimalVectorPosition;
 
-    // Update is called once per frame
-    void Update()
+    // used for setting up gates
+    private void OnDrawGizmos() 
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(optimalVectorPosition.position - optimalVectorPosition.forward * 5, optimalVectorPosition.forward * 10); 
     }
 }
