50acbd56aef407cc2326c930e2601ff6f6953b06
jransome <jamesransome@msn.com>
2019-03-01 17:41:14 +0000

    Enable gene pool saving from UI

--- Assets/Scripts/Persistence.cs
+++ Assets/Scripts/Persistence.cs
@@ -10,13 +10,14 @@ public class Persistence
     private static DirectoryInfo dirInfo = new DirectoryInfo(saveDirectory);
 
     public static List<PopulationData> GetSavedPopulations() => dirInfo.GetFiles().Select(f => ReadFile(Path.Combine(saveDirectory, f.Name))).ToList();
-    
-    public static void Save(List<Dna> genePool)
+
+    public static void Save(PopulationData popData)
     {
-        string filePath = Path.Combine(saveDirectory, GenerateFileName());
+        Debug.Log(saveDirectory);
+        string filePath = Path.Combine(saveDirectory, popData.SaveName + ".json");
         FileStream fs = File.OpenWrite(filePath);
         StreamWriter writer = new StreamWriter(fs);
-        writer.Write(JsonUtility.ToJson(new PopulationData(genePool), true));
+        writer.Write(JsonUtility.ToJson(popData, true));
         writer.Flush();
         fs.Close();
     }
@@ -26,20 +27,18 @@ public class Persistence
         StreamReader reader = new StreamReader(File.OpenRead(path));
         return JsonUtility.FromJson<PopulationData>(reader.ReadToEnd());
     }
-
-    private static string GenerateFileName() => "population_" + System.DateTime.Now.ToString("HHmmss_ddMMyyyy") + ".json";
 }
 
 [System.Serializable]
 public struct PopulationData
 {
-    public readonly string Description;
+    public readonly string SaveName;
     public readonly List<Dna> GenePool;
     public readonly DnaStructure DnaStructure;
 
-    public PopulationData(List<Dna> genePool, string description = "")
+    public PopulationData(List<Dna> genePool, string saveName = "")
     {
-        Description = description;
+        SaveName = saveName;
         GenePool = genePool;
         DnaStructure = genePool[0].structure;
     }
---
+++ Assets/Scripts/SaveView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveView : MonoBehaviour
+{
+    public InputField SaveName;
+    public Button SaveButton;
+    public God EvolutionManager;
+    
+    private void Save()
+    {
+        // for testing
+        // DnaStructure structure = new DnaStructure(2, 2, 2, 2);
+        // List<Dna> TestGenes = new List<Dna> {
+        //     new Dna(structure),
+        //     new Dna(structure),
+        //     new Dna(structure),
+        // };
+        // PopulationData data = new PopulationData(TestGenes, SaveName.text);
+        // Persistence.Save(data);
+        
+        Persistence.Save(new PopulationData(EvolutionManager.GenePool, SaveName.text));
+    }
+
+    private void Start()
+    {
+        SaveButton.onClick.AddListener(Save);
+    }
+}
